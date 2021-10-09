71b66fbbee6670bcd2b2654d818c9be4f6a21e31
shahinrahman1000 <shahinrahman1000@gmail.com>
2021-10-10 00:30:09 +0200

    search button

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,13 +1,19 @@
 <?php
 
 namespace App\Controller;
+
+
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use  Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class PropertyController extends AbstractController
 {
@@ -33,9 +39,10 @@ class PropertyController extends AbstractController
      * @Route("/biens", name="property.index")
      * @return Response
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
 
+        // insert the data manually
     //     $property = new Property();
     //     $property->setTitle('Mon premier bien')
     //             ->setPrice(200000)
@@ -58,10 +65,19 @@ class PropertyController extends AbstractController
 
 
 
-        
-       
+        $search =new PropertySearch();
+        $form = $this->createForm(PropertySearchType::class,$search);
+        $form->handleRequest($request);
+       $properties=$paginator->paginate(
+           $this->repository->findAllVisibleQuery($search),
+           $request->query->getInt('page', 1),
+           12
+
+        );
         return $this->render('property/index.html.twig', [
-            'current_menu'=>'acheter'
+            'current_menu'=>'properties',
+            'properties'=> $properties,
+            'form'=>   $form->createView()
         ]);
     }
 
--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -3,7 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Migrations\Query\Query;
+use Doctrine\ORM\Query as ORMQuery;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -22,15 +25,26 @@ class PropertyRepository extends ServiceEntityRepository
 
     /**
      * 
-     * @return Property[]
+     * @return Query
      */
     
-    public function findAllVisible(): array
+    public function findAllVisibleQuery(PropertySearch $search): ORMQuery
     {
-        return $this->findVisibleQuery()
-        ->getQuery()
-        ->getResult();
-        
+        $query= $this->findVisibleQuery();
+            if ($search->getMaxPrice()) 
+            {
+                $query = $query
+                ->andWhere('p.price<=:maxprice')
+                ->setParameter('maxprice', $search->getMaxPrice());
+            }  
+
+            if ($search->getMinSurface()) 
+            {
+                $query = $query
+                ->andWhere('p.surface>=:minsurface')
+                ->setParameter('minsurface', $search->getMinSurface());
+            }
+        return $query->getQuery();
     }
     /**
      * 
